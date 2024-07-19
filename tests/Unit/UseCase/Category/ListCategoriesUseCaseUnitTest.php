751a9c71d6e79bf9b9936a707bd7edb0fcea819a
diogokimisima <dkimisima@gmail.com>
2024-07-19 17:43:29 -0300

    first commit

---
+++ tests/Unit/UseCase/Category/ListCategoriesUseCaseUnitTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Unit\UseCase\Category;
+
+use Core\Domain\Repository\CategoryRepositoryInterface;
+use Core\Domain\Repository\PaginationInterface;
+use Core\UseCase\Category\ListCategoriesUseCase;
+use Core\UseCase\DTO\Category\ListCategories\ListCategoriesInputDto;
+use Core\UseCase\DTO\Category\ListCategories\ListCategoriesOutputDto;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+class ListCategoriesUseCaseUnitTest extends TestCase
+{
+    public function testListCategoriesEmpty()
+    {
+        $mockPagination = $this->mockPagination();
+
+        $this->mockRepo = Mockery::mock(stdClass::class, CategoryRepositoryInterface::class);
+        $this->mockRepo->shouldReceive('paginate')->andReturn($mockPagination);
+
+        $this->mockInputDto = Mockery::mock(ListCategoriesInputDto::class, ['filter', 'desc']);
+
+        $useCase = new ListCategoriesUseCase($this->mockRepo);
+        $responseUseCase = $useCase->execute($this->mockInputDto);
+
+        $this->assertCount(0, $responseUseCase->items);
+        $this->assertInstanceOf(ListCategoriesOutputDto::class, $responseUseCase);
+    }
+
+    protected function mockPagination()
+    {
+        $this->mockPagination = Mockery::mock(stdClass::class, PaginationInterface::class);
+        $this->mockPagination->shouldReceive('items')->andReturn([]);
+        $this->mockPagination->shouldReceive('total')->andReturn(0);
+        $this->mockPagination->shouldReceive('firstPage')->andReturn(0);
+        $this->mockPagination->shouldReceive('lastPage')->andReturn(0);
+        $this->mockPagination->shouldReceive('perPage')->andReturn(0);
+        $this->mockPagination->shouldReceive('to')->andReturn(0);
+        $this->mockPagination->shouldReceive('from')->andReturn(0);
+
+        return $this->mockPagination;
+    }
+
+    protected function tearDown(): void
+    {
+        Mockery::close();
+
+        parent::tearDown();
+    }
+}
