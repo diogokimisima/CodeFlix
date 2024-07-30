f12634deea4533891c09d4dcfe205a8d7b74ce0a
diogokimisima <dkimisima@gmail.com>
2024-07-30 12:49:01 -0300

    update category testing completed

--- tests/Unit/UseCase/Category/UpdateCategoryUseCaseUnitTest.php
+++ tests/Unit/UseCase/Category/UpdateCategoryUseCaseUnitTest.php
@@ -4,6 +4,9 @@ namespace Tests\Unit\UseCase\Category;
 
 use Core\Domain\Entity\Category as EntityCategory;
 use Core\Domain\Repository\CategoryRepositoryInterface;
+use Core\UseCase\Category\UpdateCategoryUseCase;
+use Core\UseCase\DTO\Category\UpdateCategory\CategoryUpdateInputDto;
+use Core\UseCase\DTO\Category\UpdateCategory\CategoryUpdateOutputDto;
 use Mockery;
 use PHPUnit\Framework\TestCase;
 use Ramsey\Uuid\Uuid;
@@ -23,21 +26,35 @@ class UpdateCategoryUseCaseUnitTest extends TestCase
             $categoryDesc
         ]);
 
-        $this->mockRepo->shouldReceive('update');
+        $this->mockEntity->shouldReceive('update');
 
-        $this->mockRepo = Mockery::mock(stdClass::class, CategoryRepositoryInterface);
+        $this->mockRepo = Mockery::mock(stdClass::class, CategoryRepositoryInterface::class);
         $this->mockRepo->shouldReceive('findById')->andReturn($this->mockEntity);
         $this->mockRepo->shouldReceive('update')->andReturn($this->mockEntity);
 
         $this->mockInputDto = Mockery::mock(
-            UpdateCategoryInputDto::class,
-            $uuid,
-            'new name',
+            CategoryUpdateInputDto::class,
+            [
+                $uuid,
+                'new name',
+            ]
         );
 
         $useCase = new  UpdateCategoryUseCase($this->mockRepo);
         $responseUseCase = $useCase->execute($this->mockInputDto);
 
-        $this->assertInstanceOf(CategoryUpdateOutputDto::class);
+        $this->assertInstanceOf(CategoryUpdateOutputDto::class, $responseUseCase);
+
+        // Spies
+
+        $this->spy = Mockery::spy(stdClass::class, CategoryRepositoryInterface::class);
+        $this->spy->shouldReceive('findById')->andReturn($this->mockEntity);
+        $this->spy->shouldReceive('update')->andReturn($this->mockEntity);
+        $useCase = new  UpdateCategoryUseCase($this->spy);
+        $useCase->execute($this->mockInputDto);
+        $this->spy->shouldHaveReceived('findById');
+        $this->spy->shouldHaveReceived('update');
+
+        Mockery::close();
     }
 }
