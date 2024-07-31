af568d40cf54ebbf8c3c3f010525a80239e622e0
diogokimisima <dkimisima@gmail.com>
2024-07-31 18:31:40 +0000

    implementando o teste do repositorio paginate

--- app/Repositories/Eloquent/CategoryEloquentRepository.php
+++ app/Repositories/Eloquent/CategoryEloquentRepository.php
@@ -55,7 +55,14 @@ class CategoryEloquentRepository implements CategoryRepositoryInterface
 
     public function paginate(string $filter = '', $order = 'DESC', int $page = 1, int $totalPage = 15): PaginationInterface
     {
-        return New PaginationPresenter();
+        $query = $this->model;
+        if ($filter) {
+            $query->where('name', 'LIKE', "%{$filter}%");
+        }
+        $query->orderBy('id', $order);
+        $paginator = $query->paginate();
+
+        return New PaginationPresenter($paginator);
     }
 
     public function update(Category $category): Category
