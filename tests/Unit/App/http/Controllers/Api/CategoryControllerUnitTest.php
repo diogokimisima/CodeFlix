e48103b9d4a6eec22eec44b01cce31ec1a73af65
diogokimisima <dkimisima@gmail.com>
2024-08-01 14:24:08 +0000

    criando o controller da listagem das categorias

---
+++ tests/Unit/App/http/Controllers/Api/CategoryControllerUnitTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Unit\App\http\Controllers\Api;
+
+use App\Http\Controllers\Api\CategoryController;
+use Core\UseCase\Category\{
+    ListCategoriesUseCase
+};
+use Core\UseCase\DTO\Category\ListCategories\ListCategoriesOutputDto;
+use Illuminate\Http\Request;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+
+class CategoryControllerUnitTest extends TestCase
+{
+    public function testIndex()
+    {
+        $mockRequest = Mockery::mock(Request::class);
+        $mockRequest->shouldReceive('get')->andReturn('teste');
+
+        $mockDtoOutput = Mockery::mock(ListCategoriesOutputDto::class, [
+            [],
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+
+        ]);
+
+        $mockUseCase = Mockery::mock(ListCategoriesUseCase::class);
+        $mockUseCase->shouldReceive('execute')->andReturn($mockDtoOutput);
+
+        $controller = new CategoryController();
+        $response = $controller->index($mockRequest, $mockUseCase);
+        dump($response);
+        $this->assertIsObject($response->resource);
+        $this->assertArrayHasKey('meta', $response->additional);
+    }
+}
