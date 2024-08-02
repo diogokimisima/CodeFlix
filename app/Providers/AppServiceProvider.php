bb7dcaf644b0cea62164bddb0c340e465703ddd8
diogokimisima <dkimisima@gmail.com>
2024-08-02 16:35:21 +0000

    trocando a instancia da classe de bind para singleton

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(
+        $this->app->singleton(
             CategoryRepositoryInterface::class,
             CategoryEloquentRepository::class
         );
