4df054a0d5b3c5686fefedc1d2de0ca97fbb0c28
diogokimisima <dkimisima@gmail.com>
2024-08-05 17:30:49 +0000

    criando o teste de desativar e ativar a entidadede um genero

--- src/Core/Domain/Entity/Genre.php
+++ src/Core/Domain/Entity/Genre.php
@@ -2,19 +2,29 @@
 
 namespace Core\Domain\Entity;
 
+use Core\Domain\Entity\Traits\MethodsMagicsTrait;
 use Core\Domain\ValueObject\Uuid;
 use DateTime;
 
 class Genre
 {
+    use MethodsMagicsTrait;
+
     public function __construct(
-        protected Uuid|null $id = null,
         protected string $name,
-        protected DateTime|null $createdAt = null,
-        protected $isActive = true
-    ) 
-    {
+        protected ?Uuid $id = null,
+        protected $isActive = true,
+        protected ?DateTime $createdAt = null,
+    ) {
         $this->id = $this->id ?? Uuid::random();
         $this->createdAt = $this->createdAt ?? new DateTime();
+    }
+
+    public function activate() {
+        $this->isActive = true;
+    }
+
+    public function deactivate() {
+        $this->isActive = false;
     }
 }
--- tests/Unit/Domain/Entity/GenreUnitTest.php
+++ tests/Unit/Domain/Entity/GenreUnitTest.php
@@ -13,12 +13,57 @@ class GenreUnitTest extends TestCase
     public function testAttributes()
     {
         $uuid = (string) RamseyUuid::uuid4();
+        $date = date('Y-m-d H:i:s');
 
         $genre = new Genre(
             id: new Uuid($uuid),
             name: 'New Genre',
-            isActive: true,
+            isActive: false,
             createdAt: new DateTime(date('Y-m-d H:i:s')),
         );
+
+        $this->assertEquals($uuid, $genre->id());
+        $this->assertEquals('New Genre', $genre->name);
+        $this->assertEquals(false, $genre->isActive);
+        $this->assertEquals($date, $genre->createdAt());
+    }
+
+    public function testAttributesCreate()
+    {
+        $genre = new Genre(
+            name: 'New Genre',
+        );
+
+        $this->assertNotEmpty($genre->id());
+        $this->assertEquals('New Genre', $genre->name);
+        $this->assertEquals(true, $genre->isActive);
+        $this->assertNotEmpty($genre->createdAt());
+    }
+
+    public function testDeactivate()
+    {
+        $genre = new Genre(
+            name: "test"
+        );
+
+        $this->assertTrue($genre->isActive);  
+        
+        $genre->deactivate();
+
+        $this->assertFalse($genre->isActive);  
+    }
+
+    public function testActivate()
+    {
+        $genre = new Genre(
+            name: "test",
+            isActive: false,
+        );
+
+        $this->assertFalse($genre->isActive);  
+
+        $genre->activate();
+
+        $this->assertTrue($genre->isActive);
     }
 }
