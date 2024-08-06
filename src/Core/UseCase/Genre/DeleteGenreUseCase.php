77da914511a9d1a950fd29fc44e44ee5454dbacf
diogokimisima <dkimisima@gmail.com>
2024-08-06 20:22:58 +0000

    criando o teste de deletar do usecase de genero

---
+++ src/Core/UseCase/Genre/DeleteGenreUseCase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Core\UseCase\Genre;
+
+use Core\Domain\Repository\GenreRepositoryInterface;
+use Core\UseCase\DTO\Genre\Delete\DeleteGenreOutputDto;
+use Core\UseCase\Dto\Genre\GenreInputDto;
+
+class DeleteGenreUseCase
+{
+    protected $repository;
+
+    public function __construct(GenreRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
+
+    public function execute(GenreInputDto $input): DeleteGenreOutputDto
+    {   
+        $success = $this->repository->delete($input->id);
+
+        return new DeleteGenreOutputDto(
+            success: $success
+        );
+    }
+}
