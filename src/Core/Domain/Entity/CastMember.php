29860a89b3ee0b3dca3e9ed3195432024a0267c4
diogokimisima <dkimisima@gmail.com>
2024-08-15 18:23:56 +0000

    criando a validacao do entity cast member

--- src/Core/Domain/Entity/CastMember.php
+++ src/Core/Domain/Entity/CastMember.php
@@ -2,19 +2,32 @@
 
 namespace Core\Domain\Entity;
 
+use Core\Domain\Entity\Traits\MethodsMagicsTrait;
 use Core\Domain\Enum\CastMemberType;
+use Core\Domain\Validation\DomainValidation;
 use Core\Domain\ValueObject\Uuid;
 use DateTime;
 
 class CastMember {
+    use MethodsMagicsTrait;
+
     public function __construct(
-        protected ?Uuid $id = null,
         protected string $name,
         protected CastMemberType $type,
+        protected ?Uuid $id = null,
         protected ?DateTime $createdAt = null,
     )
     {
-        $this->id = $this->null ?? Uuid::random();
+        $this->id = $this->id ?? Uuid::random();
         $this->createdAt = $this->createdAt ?? new DateTime();
+
+        $this->validate();
+
+    }
+
+    protected function validate()
+    {
+        DomainValidation::strMaxLength($this->name);
+        DomainValidation::strMinLength($this->name);
     }
 }
--- tests/Unit/Domain/Entity/CastMemberUnitTest.php
+++ tests/Unit/Domain/Entity/CastMemberUnitTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Domain\Entity;
 
 use Core\Domain\Enum\CastMemberType;
 use Core\Domain\Entity\CastMember;
+use Core\Domain\Exception\EntityValidationException;
 use Core\Domain\ValueObject\Uuid;
 use DateTime;
 use PHPUnit\Framework\TestCase;
@@ -11,7 +12,7 @@ use Ramsey\Uuid\Uuid as RamseyUuid;
 
 class CastMemberUnitTest extends TestCase
 {
-    public function testAttribute()
+    public function testAttributes()
     {
         $uuid = (string) RamseyUuid::uuid4();
 
@@ -20,6 +21,33 @@ class CastMemberUnitTest extends TestCase
             name: 'Name',
             type: CastMemberType::ACTOR,
             createdAt: new DateTime(date('Y-m-d H:i:s'))
+        );
+
+        $this->assertEquals($uuid, $castMember->id());
+        $this->assertEquals('Name', $castMember->name);
+        $this->assertEquals(CastMemberType::ACTOR, $castMember->type);
+        $this->assertNotEmpty($castMember->createdAt());
+    }
+
+    public function testAttributesNewEntity()
+    {
+        $castMember = new CastMember(
+            name: 'Name',
+            type: CastMemberType::DIRECTOR,
+        );
+
+        $this->assertNotEmpty($castMember->id());
+        $this->assertEquals(CastMemberType::DIRECTOR, $castMember->type);
+        $this->assertNotEmpty($castMember->createdAt());
+    }
+
+    public function testValidation()
+    {
+        $this->expectException(EntityValidationException::class);
+
+        new CastMember(
+            name: 'ab',
+            type: CastMemberType::DIRECTOR,
         );
     }
 }
