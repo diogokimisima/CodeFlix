45a15fec59941fefb909f3632fd233ecf9e81222
diogokimisima <dkimisima@gmail.com>
2024-08-16 13:25:40 +0000

    implementando o teste unitario de criacao de castmember

---
+++ src/Core/UseCase/DTO/CastMember/Create/CastMemberCreateInputDto.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Core\UseCase\DTO\CastMember\Create;
+
+class CastMemberCreateInputDto 
+{
+    public function  __construct(
+        public string $name,
+        public int $type,
+    )
+    {
+        
+    }
+}
