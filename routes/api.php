1f38e89346907a5d3e6b5a3198e272524b8c54e1
diogokimisima <dkimisima@gmail.com>
2024-08-19 20:08:01 +0000

    criando a api de cast member

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,23 @@
 <?php
 
 use App\Http\Controllers\Api\{
+    CastMemberController,
     CategoryController,
     GenreController,
 };
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/categories', CategoryController::class);
+Route::apiResource(
+    '/categories',
+    CategoryController::class
+);
 Route::apiResource(
     name: '/genres',
     controller: GenreController::class
+);
+Route::apiResource(
+    '/cast_members',
+    controller: CastMemberController::class
 );
 
 Route::get('/', function () {
