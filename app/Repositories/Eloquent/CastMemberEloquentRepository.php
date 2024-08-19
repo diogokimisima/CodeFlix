8c5024e3c51d4bf2af291558035f9b6e7083e5bd
diogokimisima <dkimisima@gmail.com>
2024-08-19 17:17:13 +0000

    implementando o teste de insert do cast member

---
+++ app/Repositories/Eloquent/CastMemberEloquentRepository.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Models\CastMember as Model;
+use Core\Domain\Entity\CastMember;
+use Core\Domain\Enum\CastMemberType;
+use Core\Domain\Repository\CastMemberRepositoryInterface;
+use Core\Domain\Repository\PaginationInterface;
+use Core\Domain\ValueObject\Uuid as ValueObjectUuid;
+
+class CastMemberEloquentRepository implements CastMemberRepositoryInterface
+{
+    protected $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    public function insert(CastMember $castMember): CastMember 
+    {
+        $dataDb = $this->model->create([
+            'id' => $castMember->id(),
+            'name' => $castMember->name,
+            'type' => $castMember->type->value,
+            'created_at' => $castMember->createdAt(),
+        ]);
+
+        return $this->convertToEntity($dataDb);
+    }
+
+    public function findById(string $castMemberId): CastMember 
+    {
+        
+    }
+
+    public function findAll(string $filter = '', $order = 'DESC'): array 
+    {
+        
+    }
+
+    public function paginate(string $filter = '', $order = 'DESC', int $page = 1, int $totalPage = 15): PaginationInterface 
+    {
+        
+    }
+
+    public function update(CastMember $castMember): CastMember 
+    {
+        
+    }
+
+    public function delete(string $castMemberId): bool 
+    {
+        
+    }
+
+
+    private function convertToEntity(Model $model): CastMember
+    {
+        return new CastMember(
+            id: new ValueObjectUuid($model->id),
+            name: $model->name,
+            type: CastMemberType::from($model->type),
+            createdAt: $model->created_at
+        );
+    }
+}
---
+++ tests/Feature/App/Repositories/CastMemberEloquentRepositoryTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature\App\Repositories;
+
+use App\Models\CastMember as Model;
+use Core\Domain\Entity\CastMember as Entity;
+use App\Repositories\Eloquent\CastMemberEloquentRepository;
+use Core\Domain\Enum\CastMemberType;
+use Core\Domain\Repository\CastMemberRepositoryInterface;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CastMemberEloquentRepositoryTest extends TestCase
+{
+    protected $repository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->repository = new CastMemberEloquentRepository(new Model());
+    }
+
+    public function testCheckImplementsInterfaceRepository()
+    {
+        $this->assertInstanceOf(CastMemberRepositoryInterface::class, $this->repository);
+    }
+
+    public function testInsert()
+    {
+        $entity = new Entity(
+            name: 'teste',
+            type: CastMemberType::ACTOR,
+        );
+
+        $response = $this->repository->insert($entity);
+
+        $this->assertDatabaseHas('cast_members', [
+            'id' => $entity->id(),
+        ]);
+
+        $this->assertEquals($entity->name ,$response-> name);
+    }
+}
